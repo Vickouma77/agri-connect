74dac30f8c7c4bea7505e41381a064ce463a0a8a
vickouma77 <Vickprestone20@gmail.com>
2023-09-12 14:14:48 +0300

    implementing cors and cookieSessions

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
-const app = express()
 const routers = require('./routers/index')
+const cors = require('cors')
+const cookieSession = require('cookie-session')
+
 const port = parseInt(process.env.PORT, 10) || 5000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieSession({
+    name: 'session',
+    keys: ['COOKIE_SECRET'],
+    httpOnly: true,
+}))
 
 app.use('/', routers)
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
+    console.log(`Server listening on port ${port}`)
 })
