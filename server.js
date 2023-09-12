345b4782e2a434f9961e39e780b6d79f06f8bafe
vickouma77 <Vickprestone20@gmail.com>
2023-09-12 11:24:51 +0300

    importing routers

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const routers = require('./routers')
+const routers = require('./routers/index')
 const port = parseInt(process.env.PORT, 10) || 5000
 
 app.use(express.json())
