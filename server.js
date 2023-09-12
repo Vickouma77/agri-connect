18ca37ad381898c94964939b8cea5b6125ea1ed8
vickouma77 <Vickprestone20@gmail.com>
2023-09-12 11:13:09 +0300

    Added the Express.js framework and set up an Express application.

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = parseInt(process.env.PORT, 10) || 5000
+
+app.use(express.json())
+app.use('/', routers)
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
