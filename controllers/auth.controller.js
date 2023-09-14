2c081096b68f7c2fc815685ab11c6ec7c5a8e5e2
vickouma77 <Vickprestone20@gmail.com>
2023-09-14 13:39:20 +0300

    quick fix

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -111,23 +111,3 @@ exports.signin = (req, res) => {
 exports.signout = (req, res) => {
     res.status(200).send({ message: 'User was signed out successfully!' })
 }
-
-exports.userContent = (req, res) => {
-    res.status(200).send({ message: 'User Content.' })
-}
-
-exports.moderatorBoard = (req, res) => {
-    res.status(200).send({ message: 'Moderator Content.' })
-}
-
-exports.adminBoard = (req, res) => {
-    res.status(200).send({ message: 'Admin Content.' })
-}
-
-exports.allAccess = (req, res) => {
-    res.status(200).send({ message: 'Public Content.' })
-}
-
-exports.userBoard = (req, res) => {
-    res.status(200).send({ message: 'User Content.' })
-}
