d0c6a381da6723c3e2127e12e42434f287e8026f
Vickouma77 <Vickprestone20@gmail.com>
2024-08-04 22:57:16 +0300

    fix(CustomAuthProvider): fix support method

--- services/auth-service/src/main/kotlin/com/agri_connect/auth_service/components/CustomAuthProvider.kt
+++ services/auth-service/src/main/kotlin/com/agri_connect/auth_service/components/CustomAuthProvider.kt
@@ -1,4 +1,28 @@
 package com.agri_connect.auth_service.components
 
-class CustomAuthProvider {
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
+import org.springframework.stereotype.Component
+
+@Component
+class CustomAuthProvider: AuthenticationProvider {
+
+    @Throws(AuthenticationException::class)
+    override fun authenticate(authentication: Authentication): Authentication? {
+        val username: String = authentication.name
+        val password: String = authentication.credentials.toString()
+
+        return if (username == "john" && password == "12345") {
+            UsernamePasswordAuthenticationToken(username, password, emptyList())
+        }else {
+            throw AuthenticationCredentialsNotFoundException("Error!")
+        }
+    }
+
+    override fun supports(authentication: Class<*>): Boolean {
+        return UsernamePasswordAuthenticationToken::class.java.isAssignableFrom(authentication)
+    }
 }
--- services/auth-service/src/main/kotlin/com/agri_connect/auth_service/controllers/HelloController.kt
+++ services/auth-service/src/main/kotlin/com/agri_connect/auth_service/controllers/HelloController.kt
@@ -1,4 +1,14 @@
 package com.agri_connect.auth_service.controllers
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
 class HelloController {
+
+    @GetMapping("/hello")
+    fun helloWorld(): String {
+        return "Hello World"
+    }
 }
